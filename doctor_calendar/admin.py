0be9f1006cc1316e2d730d3bb24f195a8f8a459f
IreneuszSzott <ireneusz.szott@onet.eu>
2021-01-14 23:40:54 +0100

    Fixed migrations for Event model

--- doctor_calendar/admin.py
+++ doctor_calendar/admin.py
@@ -2,8 +2,3 @@ from django.contrib import admin
 from doctor_calendar.models import Event
 
 admin.site.register(Event)
-
-
-class EventAdmin(admin.ModelAdmin):
-    list_display = ['day', 'start_time', 'end_time']
-
--- doctor_calendar/models.py
+++ doctor_calendar/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-# Create your models here.
+from django.contrib import admin
+
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['day', 'start_time', 'end_time']
+
